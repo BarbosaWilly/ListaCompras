779b7474e097289dee4e460a0cd6d7e366dc4002
BarbosaWilly <willybarbosacontato@gmail.com>
2026-07-14 14:36:31 -0300

    criando o primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MinhaListaComponent } from "./minha-lista/minha-lista.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MinhaListaComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
