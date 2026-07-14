a7b05276d8fb51578364546198c206122f222624
BarbosaWilly <willybarbosacontato@gmail.com>
2026-07-14 16:38:08 -0300

    Segunda parte

---
+++ src/app/minha-lista/itemList.ts
@@ -0,0 +1,6 @@
+export class ItemList {
+  //id?: number;
+  qty?: number;
+  name?: string;
+  buyed: boolean = false;
+}
--- src/app/minha-lista/minha-lista.component.ts
+++ src/app/minha-lista/minha-lista.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemList } from "./itemList";
 
 @Component({
   selector: 'app-minha-lista',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './minha-lista.component.html',
   styleUrl: './minha-lista.component.scss'
 })
 export class MinhaListaComponent {
+  item: string = '';
+  qty?: number;
+  list: ItemList[] = [];
+  isDark = false;
+
+  addItem() {
+    let itemList = new ItemList();
+    itemList.name = this.item;
+    itemList.qty = this.qty;
+    this.list.push(itemList);
+    console.table(this.list);
+    this.item = '';
+    this.qty = undefined;
+  }
+
+  toggleItemStatus(item: ItemList) {
+    item.buyed = !item.buyed;
+  }
+
+  eraseItem() {
+    this.list = [];
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark-theme', this.isDark);
+  }
 
 }
