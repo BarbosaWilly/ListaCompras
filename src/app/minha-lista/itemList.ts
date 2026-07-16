4c63479d0d1c1a43f5a5bd674cc4777b56a95350
BarbosaWilly <willybarbosacontato@gmail.com>
2026-07-16 17:47:30 -0300

    Limitando tamanhos e corrigindo bug do carregamento da página

--- src/app/minha-lista/itemList.ts
+++ src/app/minha-lista/itemList.ts
@@ -1,15 +1,5 @@
 export class ItemList {
+  qty?: number;
   name?: string;
   buyed: boolean = false;
-  
-  // Nome interno alternativo e claro
-  private internalQty?: number;
-
-  get qty(): number | undefined {
-    return this.internalQty;
-  }
-
-  set qty(value: number | undefined) {
-    this.internalQty = value && value > 1000 ? 1000 : value;
-  }
 }
